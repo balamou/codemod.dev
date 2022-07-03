39bcbb29259a6cf39d6fb9befcfd1adf3aeb6163
Michel Balamou <michel.balamou@shopify.com>
2022-07-03 00:00:55 -0400

    Split webpack config into two separate files for prod and dev

---
+++ webpack.common.js
@@ -0,0 +1,12 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/template.html",
+      hash: true,
+    }),
+  ],
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,23 +1,16 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
 
-module.exports = {
+module.exports = merge(common, {
   mode: "development",
-  entry: "./src/index.js",
   devtool: "inline-source-map", // to enable source maps
   devServer: {
     static: "./dist", // https://webpack.js.org/guides/development/#using-webpack-dev-server
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/template.html",
-      hash: true,
-    }),
-  ],
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.[contenthash].js",
+    filename: "main.js", // no content hash here for dev
     clean: true, // clean output folder anytime we build
   },
-};
+});
