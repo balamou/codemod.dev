6d9696935c9bb0050088abc9d191d565d15081c1
Michel Balamou <michel.balamou@shopify.com>
2022-07-03 00:39:48 -0400

    Add Terser JS minifier

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const common = require('./webpack.common');
 const {merge} = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -8,5 +9,9 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.[contenthash].js',
     clean: true, // clean output folder anytime we build
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
 });
